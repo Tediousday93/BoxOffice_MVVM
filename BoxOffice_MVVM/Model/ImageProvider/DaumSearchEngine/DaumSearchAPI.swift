60f3112a86fd5f0369428f544c19442753f36d15
ROWAN <kimkj93@naver.com>
2024-02-27 15:55:37 +0900

    feat: DaumImageSearchAPI 타입 정의

---
+++ BoxOffice_MVVM/Model/ImageProvider/DaumSearchEngine/DaumSearchAPI.swift
@@ -0,0 +1,69 @@
+//
+//  DaumSearchAPI.swift
+//  BoxOffice_MVVM
+//
+//  Created by Rowan on 2024/02/27.
+//
+
+import Foundation
+
+struct DaumImageSearchAPI: APIConfigurationType {
+    typealias Response = DaumImageSearchResult
+    
+    var baseURL: String { "https://dapi.kakao.com/v2" }
+    
+    var path: String { "/search/image" }
+    
+    var method: HTTPMethod { .get }
+    
+    var headers: [String : String] {
+        guard let key = Bundle.main.infoDictionary?["DAUM_SEARCH_API_KEY"] as? String
+        else { fatalError("Could not find DAUM_SEARCH_API_KEY from main bundle's infoDictionary") }
+        
+        return [
+            "Authorization": key
+        ]
+    }
+    
+    var queryParameters: [String : Any] = [:]
+    
+    var bodyParameters: [String : Any] = [:]
+    
+    mutating func addQuery(parameters: QueryParameter...) {
+        parameters.forEach { parameter in
+            queryParameters[parameter.key] = parameter.value
+        }
+    }
+}
+
+extension DaumImageSearchAPI {
+    enum QueryParameter {
+        case query(keyword: String)
+        case sort(method: Sorting)
+        case page(number: Int)
+        case size(Int)
+        
+        var key: String {
+            switch self {
+            case .query: return "query"
+            case .sort: return "sort"
+            case .page: return "page"
+            case .size: return "size"
+            }
+        }
+        
+        var value: Any {
+            switch self {
+            case let .query(keyword): return keyword
+            case let .sort(method): return method
+            case let .page(number): return number
+            case let .size(count): return count
+            }
+        }
+    }
+        
+    enum Sorting: String {
+        case accuracy
+        case recency
+    }
+}
