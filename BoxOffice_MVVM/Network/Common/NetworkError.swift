fa632a83054a206a134d06128808575241f1f1d8
ROWAN <kimkj93@naver.com>
2024-02-27 15:26:30 +0900

    refactor: NetworkError - case 이름 변경 및 foundation import 삭제

--- BoxOffice_MVVM/Network/Common/NetworkError.swift
+++ BoxOffice_MVVM/Network/Common/NetworkError.swift
@@ -5,10 +5,8 @@
 //  Created by Rowan on 2024/02/20.
 //
 
-import Foundation
-
 enum NetworkError: Error {
-    case unknownError
+    case unknown
     case responseNotFound
     case invalidHttpStatusCode(statusCode: Int)
     case emptyData
