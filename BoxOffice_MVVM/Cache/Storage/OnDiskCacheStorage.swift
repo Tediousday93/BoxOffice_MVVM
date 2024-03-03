ca2b3c17efecd737d4b5bed72a5c0858ccffda06
ROWAN <kimkj93@naver.com>
2024-03-03 16:14:31 +0900

    feat: OnDiskCacheStorage 캐시 만료 기간 설정 코드 추가, extendExpiration 메서드 정의

--- BoxOffice_MVVM/Cache/Storage/OnDiskCacheStorage.swift
+++ BoxOffice_MVVM/Cache/Storage/OnDiskCacheStorage.swift
@@ -8,38 +8,47 @@
 import Foundation
 
 enum OnDiskCacheError: Error {
-    case cannotCreateDirectory(error: Error)
+    case cannotCreateDirectory(path: String, error: Error)
+    
     case storageNotReady
-    case cannotCreateFile(error: Error)
-    case cannotFindValue(error: Error)
+    
+    case cannotCreateFile(url: URL, error: Error)
+    
+    case cannotSetFileAttributes(filePath: String, attributes: [FileAttributeKey: Any], error: Error)
+    
+    case invalidURLResource(keys: Set<URLResourceKey>, url: URL, error: Error)
+    
+    case cannotRemoveFile(url: URL, error: Error)
+    
+    case cannotFindValue(url: URL, error: Error)
 }
 
 final class OnDiskCacheStorage {
-    private let storage: FileManager
+    private let fileManager: FileManager
     
     private let directoryURL: URL
     
     private var isStorageReady: Bool = true
     
-    init(storage: FileManager = .default) {
-        self.storage = storage
-        self.directoryURL = storage.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+    init(fileManager: FileManager = .default) {
+        self.fileManager = fileManager
+        self.directoryURL = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
         try? prepareDirectory()
     }
     
     private func prepareDirectory() throws {
         let path = directoryURL.path
         
-        guard !storage.fileExists(atPath: path) else { return }
+        guard !fileManager.fileExists(atPath: path) else { return }
         
         do {
-            try storage.createDirectory(
+            try fileManager.createDirectory(
                 atPath: path,
                 withIntermediateDirectories: true
             )
         } catch {
             isStorageReady = false
-            throw OnDiskCacheError.cannotCreateDirectory(error: error)
+            throw OnDiskCacheError.cannotCreateDirectory(path: path, error: error)
         }
     }
     
@@ -53,17 +62,82 @@ final class OnDiskCacheStorage {
         do {
             try value.write(to: fileURL)
         } catch {
-            throw OnDiskCacheError.cannotCreateFile(error: error)
+            throw OnDiskCacheError.cannotCreateFile(url: fileURL, error: error)
+        }
+        
+        let now = Date()
+        let estimatedExpiration = TimeInterval(3600 * 24) * TimeInterval(7)
+        let expirationDate = now.addingTimeInterval(estimatedExpiration)
+        let attributes: [FileAttributeKey: Any] = [
+            .creationDate: now as NSDate,
+            .modificationDate: expirationDate as NSDate
+        ]
+        
+        do {
+            try fileManager.setAttributes(attributes, ofItemAtPath: fileURL.path)
+        } catch {
+            try? fileManager.removeItem(at: fileURL)
+            throw OnDiskCacheError.cannotSetFileAttributes(
+                filePath: fileURL.path,
+                attributes: attributes,
+                error: error
+            )
         }
     }
     
-    func value(for key: String) throws -> Data {
-        let filePath = directoryURL.appending(path: key)
+    func value(for key: String) throws -> Data? {
+        let fileURL = directoryURL.appending(path: key)
+        
+        guard fileManager.fileExists(atPath: fileURL.path) else {
+            return nil
+        }
+        
+        let resourceKeys: Set<URLResourceKey> = [.creationDateKey, .contentModificationDateKey]
+        let urlResourceValues: URLResourceValues
+        
         do {
-            let value = try Data(contentsOf: filePath)
+            urlResourceValues = try fileURL.resourceValues(forKeys: resourceKeys)
+        } catch {
+            throw OnDiskCacheError.invalidURLResource(
+                keys: resourceKeys, url: fileURL, error: error
+            )
+        }
+        
+        let isExpired = urlResourceValues.contentModificationDate?.isPast(referenceDate: Date()) ?? true
+        
+        if isExpired { return nil }
+        
+        do {
+            let value = try Data(contentsOf: fileURL)
+            extendExpiration(
+                filePath: fileURL.path,
+                lastAccessDate: urlResourceValues.creationDate,
+                lastEstimatedExpirationDate: urlResourceValues.contentModificationDate
+            )
             return value
         } catch {
-            throw OnDiskCacheError.cannotFindValue(error: error)
+            throw OnDiskCacheError.cannotFindValue(url: fileURL, error: error)
         }
+    }
+    
+    func extendExpiration(filePath: String, lastAccessDate: Date?, lastEstimatedExpirationDate: Date?) {
+        guard let lastAccessDate, let lastEstimatedExpirationDate else {
+            return
+        }
+        
+        let originalExpiration = lastEstimatedExpirationDate.timeIntervalSince(lastAccessDate)
+        let expirationDate = Date().addingTimeInterval(originalExpiration)
+        let attributes: [FileAttributeKey: Any] = [
+            .creationDate: Date() as NSDate,
+            .modificationDate: expirationDate as NSDate
+        ]
+        
+        try? fileManager.setAttributes(attributes, ofItemAtPath: filePath)
+    }
+}
+
+extension Date {
+    func isPast(referenceDate: Date) -> Bool {
+        self.timeIntervalSince(referenceDate) <= 0
     }
 }
