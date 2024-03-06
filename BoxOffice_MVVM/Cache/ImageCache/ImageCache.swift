3d99176d3559ed6a880b82f71774ec7760102963
ROWAN <kimkj93@naver.com>
2024-03-06 20:01:00 +0900

    feat: ImageCache 정의 및 store, retrieve 메서드 구현

---
+++ BoxOffice_MVVM/Cache/ImageCache/ImageCache.swift
@@ -0,0 +1,92 @@
+//
+//  ImageCache.swift
+//  BoxOffice_MVVM
+//
+//  Created by Rowan on 2024/03/05.
+//
+
+import Foundation
+import UIKit.UIImage
+
+typealias Image = UIImage
+
+enum CacheOption {
+    case all
+    case memory
+    case disk
+    case none
+}
+
+enum ImageCacheError: Error {
+    case cannotSerializeImage
+    case expiredImage(key: String)
+}
+
+final class ImageCache {
+    static let `default`: ImageCache = .init(memoryCacheLimit: 30, diskCacheLimit: 100)
+    
+    private let memoryStorage: InMemoryCacheStorage
+    private let diskStorage: OnDiskCacheStorage
+    
+    convenience init(memoryCacheLimit: Int, diskCacheLimit: Int) {
+        self.init(
+            memoryStorage: .init(countLimit: memoryCacheLimit),
+            diskStorage: .init(countLimit: diskCacheLimit)
+        )
+    }
+    
+    init(
+        memoryStorage: InMemoryCacheStorage,
+        diskStorage: OnDiskCacheStorage
+    ) {
+        self.memoryStorage = memoryStorage
+        self.diskStorage = diskStorage
+    }
+    
+    func store(_ image: Image, for key: String, option: CacheOption) throws {
+        switch option {
+        case .all:
+            storeToMemory(image, for: key)
+            try storeToDisk(image, for: key)
+        case .memory:
+            storeToMemory(image, for: key)
+        case .disk:
+            try storeToDisk(image, for: key)
+        case .none:
+            return
+        }
+    }
+    
+    private func storeToMemory(_ image: Image, for key: String) {
+        memoryStorage.store(image, for: key)
+    }
+    
+    private func storeToDisk(_ image: Image, for key: String) throws {
+        guard let imageData = image.jpegData(compressionQuality: 1.0)
+        else { throw ImageCacheError.cannotSerializeImage }
+        try diskStorage.store(value: imageData, for: key)
+    }
+    
+    func retrieveImage(for key: String) throws -> Image? {
+        if let image = retrieveImageInMemory(for: key) {
+            return image
+        }
+        
+        if let image = try retrieveImageInDisk(for: key) {
+            storeToMemory(image, for: key)
+            return image
+        }
+        
+        return nil
+    }
+    
+    private func retrieveImageInMemory(for key: String) -> Image? {
+        return memoryStorage.value(for: key)
+    }
+    
+    private func retrieveImageInDisk(for key: String) throws -> Image? {
+        guard let imageData = try diskStorage.value(for: key)
+        else { throw ImageCacheError.expiredImage(key: key) }
+        return Image(data: imageData)
+    }
+}
