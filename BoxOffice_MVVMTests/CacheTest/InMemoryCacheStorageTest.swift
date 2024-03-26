00f14aa9f1998051b4a032fa47ceae160ec478f4
ROWAN <kimkj93@naver.com>
2024-03-26 20:49:54 +0900

    feat: InMemoryCacheStorageTest 테스트 케이스 추가
    값이 저장된 키에 다시 저장하기(overwrite)
    캐시 만료기간을 직접 설정해 저장하기
    값을 가져올 때 만료기간 연장하기
    만료된 캐시 데이터 삭제하기

--- BoxOffice_MVVMTests/CacheTest/InMemoryCacheStorageTest.swift
+++ BoxOffice_MVVMTests/CacheTest/InMemoryCacheStorageTest.swift
@@ -30,4 +30,72 @@ class InMemoryCacheStorageTest: XCTestCase {
         XCTAssertTrue(memoryStorage.isCached(for: key))
         XCTAssertEqual(memoryStorage.value(for: key), value)
     }
+    
+    func test_storeValueOverwrite() {
+        let key = "one"
+        let value = 1
+        let overwriteValue = 2
+        memoryStorage.store(value, for: key)
+        XCTAssertEqual(memoryStorage.value(for: key), value)
+        
+        memoryStorage.store(overwriteValue, for: key)
+        XCTAssertEqual(memoryStorage.value(for: key), overwriteValue)
+    }
+    
+    func test_storeWithExpiration() {
+        let expectation = XCTestExpectation(description: "storeWithExpiration Expectation")
+        
+        let key = "one"
+        let value = 1
+        let expiration = TimeInterval(0.5)
+        memoryStorage.store(value, for: key, expiration: expiration)
+        
+        let cached = memoryStorage.value(for: key, extendingExpiration: false)
+        XCTAssertTrue(memoryStorage.isCached(for: key))
+        XCTAssertEqual(cached, value)
+        
+        delay(0.5) {
+            XCTAssertFalse(self.memoryStorage.isCached(for: key))
+            XCTAssertNil(self.memoryStorage.value(for: key))
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func test_getValueWithExtendingExpiration() {
+        let expectation = XCTestExpectation(description: "getValueWithExtendingExpiration Expectation")
+        
+        let key = "one"
+        let value = 1
+        memoryStorage.store(value, for: key, expiration: 0.5)
+        XCTAssertTrue(memoryStorage.isCached(for: key))
+        XCTAssertNotNil(memoryStorage.value(for: key, extendingExpiration: true))
+        
+        delay(0.5) {
+            
+        }
+    }
+    
+    func test_removeExpired() {
+        let expectation = XCTestExpectation(description: "removeExpired Expectation")
+        
+        let (firstKey, firstValue) = ("one", 1)
+        let (secondKey, secondValue) = ("two", 2)
+        memoryStorage.store(firstValue, for: firstKey, expiration: 0.2)
+        memoryStorage.store(secondValue, for: secondKey, expiration: 5)
+        XCTAssertTrue(memoryStorage.isCached(for: firstKey))
+        XCTAssertTrue(memoryStorage.isCached(for: secondKey))
+        
+        delay(0.3) {
+            self.memoryStorage.removeExpired()
+            XCTAssertFalse(self.memoryStorage.isCached(for: firstKey))
+            XCTAssertTrue(self.memoryStorage.isCached(for: secondKey))
+            XCTAssertEqual(self.memoryStorage.value(for: secondKey), secondValue)
+            XCTAssertNil(self.memoryStorage.value(for: firstKey))
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1)
+    }
 }
