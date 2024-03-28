a6d5d1fc40c3e4a6bde4867c829e0938e6df47dd
ROWAN <kimkj93@naver.com>
2024-03-28 20:28:49 +0900

    feat: ExpirationExtending 열거형 추가
    DataConvertible, ExpirationExtending Helper 폴더 하위로 위치 변경

---
+++ BoxOffice_MVVM/Cache/Helper/ExpirationExtending.swift
@@ -0,0 +1,14 @@
+//
+//  ExpirationExtending.swift
+//  BoxOffice_MVVM
+//
+//  Created by Rowan on 2024/03/28.
+//
+
+import Foundation
+
+enum ExpirationExtending {
+    case none
+    
+    case extend(second: TimeInterval)
+}
