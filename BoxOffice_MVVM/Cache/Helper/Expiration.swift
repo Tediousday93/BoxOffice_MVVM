819404683b5370615e9c417e1b954cbc2de8e826
ROWAN <kimkj93@naver.com>
2024-03-29 15:38:56 +0900

    feat: ExpirationExtending 파일명 Expiration으로 변경 및 CacheExpiration 열거형 정의

---
+++ BoxOffice_MVVM/Cache/Helper/Expiration.swift
@@ -0,0 +1,30 @@
+//
+//  Expiration.swift
+//  BoxOffice_MVVM
+//
+//  Created by Rowan on 2024/03/28.
+//
+
+import Foundation
+
+enum ExpirationExtending {
+    case none
+    
+    case extend(second: TimeInterval)
+}
+
+enum CacheExpiration {
+    case seconds(TimeInterval)
+    
+    case days(Int)
+    
+    func estimatedExpirationSince(_ date: Date) -> Date {
+        switch self {
+        case .seconds(let seconds):
+            return date.addingTimeInterval(seconds)
+        case .days(let days):
+            let duration = TimeInterval(3600 * 24 * days)
+            return date.addingTimeInterval(duration)
+        }
+    }
+}
