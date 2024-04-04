08777a2217ccb0530aaaf1c0b6da88f0d0331e6b
ROWAN <kimkj93@naver.com>
2024-04-04 17:35:16 +0900

    feat: OnDiskCacheStorageTest 정의, storeAndGetValue 테스트 케이스 추가

---
+++ BoxOffice_MVVMTests/CacheTest/OnDiskCacheStorageTest.swift
@@ -0,0 +1,52 @@
+//
+//  OnDiskCacheStorageTest.swift
+//  BoxOffice_MVVMTests
+//
+//  Created by Rowan on 2024/04/01.
+//
+
+import XCTest
+@testable import BoxOffice_MVVM
+
+extension String: DataConvertible {
+    public func toData() throws -> Data {
+        return data(using: .utf8)!
+    }
+    
+    public static func fromData(_ data: Data) throws -> String {
+        return String(data: data, encoding: .utf8)!
+    }
+    
+    public static var empty: String {
+        return ""
+    }
+}
+
+class OnDiskCacheStorageTest: XCTestCase {
+    let innerStorage = FileManager.default
+    var diskStorage: OnDiskCacheStorage<String>!
+    
+    override func setUpWithError() throws {
+        diskStorage = try .init(countLimit: 5)
+    }
+    
+    override func tearDownWithError() throws {
+        try diskStorage.removeAll()
+        diskStorage = nil
+    }
+    
+    func test_storeAndGetValue() {
+        let (key, value) = ("1", "1")
+        do {
+            XCTAssertFalse(diskStorage.isCached(for: key))
+            XCTAssertNil(try diskStorage.value(for: key))
+            
+            try diskStorage.store(value: value, for: key)
+            
+            XCTAssertTrue(diskStorage.isCached(for: key))
+            XCTAssertEqual(try diskStorage.value(for: key), value)
+        } catch {
+            XCTFail("Not Expected Error: \(error)")
+        }
+    }
+}
