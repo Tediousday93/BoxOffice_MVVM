1ec80086af9cf563ad694a5a6253629dc9a538e6
ROWAN <kimkj93@naver.com>
2024-04-10 17:21:32 +0900

    faet: ImageProviderTest - fetchImageWithDownload, fetchImageConvertingFailure 케이스 추가

--- BoxOffice_MVVM/Model/ImageProvider/ImageProvider.swift
+++ BoxOffice_MVVM/Model/ImageProvider/ImageProvider.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit.UIImage
 
-enum ImageProviderError: Error {
+enum ImageProviderError: Error, Equatable {
     case imageConvertingFail(imageURL: URL)
 }
 
--- BoxOffice_MVVMTests/ImageProviderTest/ImageProviderTest.swift
+++ BoxOffice_MVVMTests/ImageProviderTest/ImageProviderTest.swift
@@ -38,6 +38,32 @@ class ImageProviderTest: XCTestCase {
         expectation = nil
     }
     
+    func test_fetchImageWithDownload() {
+        MockURLProtocol.requestHandler = { request in
+            guard let url = request.url else {
+                throw NetworkError.invalidURL
+            }
+            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+            return (response, MockData.sampleImageData)
+        }
+        
+        let cacheKey = dummyURL.cacheKey
+        
+        imageProvider.fetchImage(from: dummyURL) { result in
+            switch result {
+            case let .success(image):
+                XCTAssertEqual(image.jpegData(compressionQuality: 1.0), self.sampleImage.jpegData(compressionQuality: 1.0))
+                XCTAssertTrue(self.mockCache.isCached(for: cacheKey))
+                
+                self.expectation.fulfill()
+            case let .failure(error):
+                XCTFail("Unexpected Error: \(error)")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 2)
+    }
+    
     func test_fetchCachedImage() {
         let cacheKey = dummyURL.cacheKey
         try! mockCache.store(sampleImage, for: cacheKey)
@@ -47,7 +73,7 @@ class ImageProviderTest: XCTestCase {
             case let .success(image):
                 XCTAssertTrue(self.mockCache.isCacheHit)
                 XCTAssertTrue(self.mockCache.isCached(for: cacheKey))
-                XCTAssertEqual(image, self.sampleImage)
+                XCTAssertEqual(image.jpegData(compressionQuality: 1.0), self.sampleImage.jpegData(compressionQuality: 1.0))
                 
                 self.expectation.fulfill()
             case let .failure(error):
@@ -56,5 +82,30 @@ class ImageProviderTest: XCTestCase {
         }
         
         wait(for: [expectation], timeout: 1)
+    }
+    
+    func test_fetchImageConvertingFailure() {
+        MockURLProtocol.requestHandler = { request in
+            guard let url = request.url else {
+                throw NetworkError.invalidURL
+            }
+            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+            return (response, Data())
+        }
+        
+        imageProvider.fetchImage(from: dummyURL) { result in
+            switch result {
+            case .success:
+                XCTFail("This request must throw error")
+            case let .failure(error):
+                if let error = error as? ImageProviderError {
+                    XCTAssertEqual(error, .imageConvertingFail(imageURL: self.dummyURL))
+                }
+                
+                self.expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 2)
     }
 }
