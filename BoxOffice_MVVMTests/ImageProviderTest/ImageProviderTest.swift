a70018b4bc3cc96cfa2cd9594ccfce88d8edc9fb
ROWAN <kimkj93@naver.com>
2024-04-10 16:29:08 +0900

    feat: UnitTest - ImageProviderTest, MockImageCache 정의

---
+++ BoxOffice_MVVMTests/ImageProviderTest/ImageProviderTest.swift
@@ -0,0 +1,13 @@
+//
+//  ImageProviderTest.swift
+//  BoxOffice_MVVMTests
+//
+//  Created by Rowan on 2024/04/09.
+//
+
+import XCTest
+@testable import BoxOffice_MVVM
+
+class ImageProviderTest: XCTestCase {
+    
+}
---
+++ BoxOffice_MVVMTests/Mock/MockImageCache.swift
@@ -0,0 +1,42 @@
+//
+//  MockImageCache.swift
+//  BoxOffice_MVVMTests
+//
+//  Created by Rowan on 2024/04/10.
+//
+
+@testable import BoxOffice_MVVM
+
+final class MockImageCache: ImageCacheType {
+    var storage: [String: Image] = [:]
+    var isCacheExpired: Bool = false
+    var isCacheHit: Bool = false
+    
+    init(
+        storage: [String: Image],
+        isCacheExpired: Bool
+    ) {
+        self.storage = storage
+        self.isCacheExpired = isCacheExpired
+    }
+    
+    func store(_ image: Image, for key: String, option: CacheOption?) throws {
+        storage[key] = image
+    }
+    
+    func retrieveImage(for key: String) throws -> BoxOffice_MVVM.Image? {
+        isCacheHit = true
+        return storage[key]
+    }
+    
+    func removeExpired(option: CacheOption?) throws {
+        if isCacheExpired {
+            storage.removeAll()
+        }
+    }
+    
+    func isCached(for key: String) -> Bool {
+        if isCacheExpired { return false }
+        return storage[key] != nil
+    }
+}
