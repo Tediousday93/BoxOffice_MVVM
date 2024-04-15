64d45ea6303f2c272f9fc99e870fc12ce15b2292
ROWAN <kimkj93@naver.com>
2024-04-15 20:01:12 +0900

    feat: DailyBoxOfficeViewController - collection view 추가

--- BoxOffice_MVVM/Presentation/DailyBoxOfficeView/DailyBoxOfficeViewController.swift
+++ BoxOffice_MVVM/Presentation/DailyBoxOfficeView/DailyBoxOfficeViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 final class DailyBoxOfficeViewController: UIViewController {
-
+    private let collectionView: UICollectionView = {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let collectionViewLayout = UICollectionViewCompositionalLayout.list(using: configuration)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setSubviews()
+        setConstraints()
     }
 
-
+    private func setSubviews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
 
