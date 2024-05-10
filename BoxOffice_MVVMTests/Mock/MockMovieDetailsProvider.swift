ffc1f7618b5e25d0e1cd6b0aab251283c20d25c3
ROWAN <kimkj93@naver.com>
2024-05-10 16:44:36 +0900

    feat: MockMovieDetailsProvider 정의, MovieDetailsViewControllerTest 파일생성

---
+++ BoxOffice_MVVMTests/Mock/MockMovieDetailsProvider.swift
@@ -0,0 +1,24 @@
+//
+//  MockMovieDetailsProvider.swift
+//  BoxOffice_MVVMTests
+//
+//  Created by Rowan on 5/10/24.
+//
+
+import Foundation
+@testable import BoxOffice_MVVM
+
+final class MockMovieDetailsProvider: MovieDetailsProvidable {
+    var getMovieDetailsCallCount = 0
+    var willThrowNetworkError: Bool = false
+    
+    func getMovieDetails(movieCode: String, completion: @escaping (Result<MovieDetails, any Error>) -> Void) {
+        if willThrowNetworkError {
+            completion(.failure(NetworkError.unknown))
+            return
+        }
+        
+        let movieDetails = try! JSONDecoder().decode(MovieDetails.self, from: MockData.movieDetails)
+        completion(.success(movieDetails))
+    }
+}
---
+++ BoxOffice_MVVMTests/ViewModelTest/MovieDetailsViewModelTest.swift
@@ -0,0 +1,14 @@
+//
+//  MovieDetailsViewModelTest.swift
+//  BoxOffice_MVVMTests
+//
+//  Created by Rowan on 5/10/24.
+//
+
+import XCTest
+@testable import BoxOffice_MVVM
+
+class MovieDetailsViewModelTest: XCTestCase {
+    var sut: MovieDetailsViewModel!
+    var mockMovieDetailsProvider: 
+}
