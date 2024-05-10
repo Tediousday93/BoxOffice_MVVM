fa73e5bdbb1c1cb8e48d26d13888f944eb47a8de
ROWAN <kimkj93@naver.com>
2024-05-10 19:05:15 +0900

    style: 컨벤션 수정 CalendarViewModel

--- BoxOffice_MVVM/Presentation/CalendarView/CalendarViewModel.swift
+++ BoxOffice_MVVM/Presentation/CalendarView/CalendarViewModel.swift
@@ -24,6 +24,7 @@ final class CalendarViewModel {
         if let startDate = dateFormatter.date(from: Constant.dailyBoxOfficeStartDate) {
             return DateInterval(start: startDate, end: DateConstant.yesterday)
         }
+        
         return nil
     }
     
